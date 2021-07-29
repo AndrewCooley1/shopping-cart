ecc8848000d48d3f1bba6b1066f98bfc86d535a0
AndrewCooley1 <ac8896@stern.nyu.edu>
2021-07-28 20:11:34 -0400

    Efficiency

--- shopping_cart.py
+++ shopping_cart.py
@@ -30,18 +30,6 @@ products = [
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
 
-def to_usd(my_price):
-    """
-    Converts a numeric value to usd-formatted string, for printing and display purposes.
-
-    Param: my_price (int or float) like 4000.444444
-
-    Example: to_usd(4000.444444)
-
-    Returns: $4,000.44
-    """
-    return f"${my_price:,.2f}" #> $12,000.71
-
 
 # TODO: write some Python code here to produce the desired output
 
