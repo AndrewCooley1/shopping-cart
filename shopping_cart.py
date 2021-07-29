9b6ce30313295e42b38a60ae50e585065193fe76
AndrewCooley1 <ac8896@stern.nyu.edu>
2021-07-28 20:14:24 -0400

    Update shopping_cart.py

--- shopping_cart.py
+++ shopping_cart.py
@@ -29,15 +29,15 @@ products = [
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
   
-"""
-    Converts a numeric value to usd-formatted string, for printing and display purposes.
 
-    Param: my_price (int or float) like 4000.444444
+    #Converts a numeric value to usd-formatted string, for printing and display purposes.
 
-    Example: to_usd(4000.444444)
+    #Param: my_price (int or float) like 4000.444444
 
-    Returns: $4,000.44
-    """
+    #Example: to_usd(4000.444444)
+
+    #Returns: $4,000.44
+    
 def to_usd(my_price): 
     return f"${my_price:,.2f}" #> $12,000.71
 
